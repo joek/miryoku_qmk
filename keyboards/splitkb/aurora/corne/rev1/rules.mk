b76e3d755fdd93ddcf4028ce2f6a3b43fe1d1bdf
Johannes Engelke <johannes.engelke@sap.com>
2024-01-26 08:26:33 +0100

    [miryoku-github] Add build file

--- keyboards/splitkb/aurora/corne/rev1/rules.mk
+++ keyboards/splitkb/aurora/corne/rev1/rules.mk
@@ -15,7 +15,3 @@
 
 # Although no rules are defined,
 # presence of this file is required for QMK to compile it.
-
-
-# Convert from Elite-C to Liatris
-CONVERT_TO=liatris
--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,5 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+# Convert from Elite-C to Liatris
+CONVERT_TO=liatris
